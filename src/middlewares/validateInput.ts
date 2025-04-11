0e0c08bb3cf9dd8e41d4ea6af7b5723867863923
Santos2175 <santoshgurung949@gmail.com>
2025-04-11 11:08:39 +0545

    feat(all): Input validation middleware added

---
+++ src/middlewares/validateInput.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express';
+import { ObjectSchema } from 'joi';
+
+import { AppError } from './errorHandler';
+
+// middleware for input validation
+export const validateInput = (schema: ObjectSchema) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { error } = schema.validate(req.body, { abortEarly: false });
+
+    if (error) {
+      // format the errors
+      const formattedErrors = error.details.map((detail) => {
+        const message = detail.message.replace(/['"]/g, '');
+        return message.charAt(0).toUpperCase() + message.slice(1);
+      });
+
+      // pass the errors to global error handler
+      next(new AppError(formattedErrors, 400));
+    }
+
+    // if there are no errors, pass the req inputs to next middleware function
+    next();
+  };
+};
